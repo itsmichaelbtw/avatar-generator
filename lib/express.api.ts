3aaa91da0171b4b92ce23192c87ee461d354f966
Michael <me@miikey.pw>
2022-08-04 19:58:07 +1000

    added redirect on base url on 404 and base url

--- lib/express.api.ts
+++ lib/express.api.ts
@@ -3,12 +3,20 @@ import { normalizeQuery, buildSVG, port, AvatarProperties } from "./utils";
 
 const app: Application = express();
 
+app.get("/", (req: Request, res: Response): void => {
+    res.redirect("/avatar.svg");
+});
+
 app.get("/avatar.(svg|png)", (req: Request, res: Response): void => {
     const svgProperties: AvatarProperties = normalizeQuery(req.query);
     const svg: string = buildSVG(svgProperties);
 
     res.set("Content-Type", "image/svg+xml");
     res.send(svg);
+});
+
+app.use("/", (req: Request, res: Response): void => {
+    res.redirect("/avatar.svg");
 });
 
 app.listen(port, (): void => {
